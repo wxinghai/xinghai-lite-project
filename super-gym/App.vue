a9cd22d50c8ca9c078f1ddad35d815c826fe3703
xinghai <1264111737@qq.com>
2025-08-10 13:01:22 +0800

    1

---
+++ super-gym/App.vue
@@ -0,0 +1,21 @@
+<script>
+	export default {
+		globalData:{
+		  statusBarHeight:''
+		},
+		onLaunch() {
+			const { statusBarHeight = 0} = uni.getSystemInfoSync()
+			this.globalData.statusBarHeight =  statusBarHeight+"px"
+		},
+		onShow: function() {
+			console.log('App Show')
+		},
+		onHide: function() {
+			console.log('App Hide')
+		}
+	}
+</script>
+
+<style>
+	/*每个页面公共css */
+</style>
